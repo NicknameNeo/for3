727319ea5a79f23f2deb0afa0f5fa2e65c70bc2f
NicknameNeo <abdulloevaslan96@gmail.com>
2024-12-04 18:54:35 +0500

    первый коммит

---
+++ for2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace for2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            // if (a >= b)
+            // {
+            //     Console.WriteLine("Число A должно быть меньше числа B.");
+            //     return;
+            // }
+            // int i = 0;
+            
+            
+            // for (int i = a; i <= b; i++)
+            // {
+            //     Console.WriteLine(i);
+            //     // i++;
+            // }
+
+            for (int i = b - 1; i > a; i--)
+            {
+                Console.WriteLine(i);
+            }    
+            
+        }
+    }
+}
